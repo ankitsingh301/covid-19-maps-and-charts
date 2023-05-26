e63acd055325266f55116d0dbf842a83899d72fa
itzankit <itzankit301@gmail.com>
2023-05-26 23:10:22 +0530

    minor changes

--- src/App.tsx
+++ src/App.tsx
@@ -1,22 +1,21 @@
 import ChartsandMaps from "./pages/ChartsandMaps";
 import Contact from "./pages/Contact";
 import { QueryClient, QueryClientProvider } from "react-query";
-import { HashRouter, Route, Routes } from "react-router-dom";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
 import Home from "./pages/Home";
 
 const queryClient = new QueryClient();
-const baseurl = process.env.PUBLIC_URL;
 
 const App: React.FC = () => {
   return (
     <QueryClientProvider client={queryClient}>
-      <HashRouter>
+      <BrowserRouter>
         <Routes>
           <Route path="/" element={<Home />}></Route>
           <Route path="/contact" element={<Contact />}></Route>
           <Route path="/charts-and-maps" element={<ChartsandMaps />}></Route>
         </Routes>
-      </HashRouter>
+      </BrowserRouter>
     </QueryClientProvider>
   );
 };
