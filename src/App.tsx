59f2d50002e2c1c594f2581d99edaf14a06a487f
itzankit <itzankit301@gmail.com>
2023-05-26 22:25:29 +0530

    minor changes

--- src/App.tsx
+++ src/App.tsx
@@ -5,15 +5,19 @@ import { BrowserRouter, Route, Routes } from "react-router-dom";
 import Home from "./pages/Home";
 
 const queryClient = new QueryClient();
+const baseurl = process.env.PUBLIC_URL;
 
 const App: React.FC = () => {
   return (
     <QueryClientProvider client={queryClient}>
-      <BrowserRouter basename={process.env.PUBLIC_URL}>
+      <BrowserRouter basename={baseurl}>
         <Routes>
-          <Route path="/" element={<Home />}></Route>
-          <Route path="/contact" element={<Contact />}></Route>
-          <Route path="/charts-and-maps" element={<ChartsandMaps />}></Route>
+          <Route path={`${baseurl}"/"`} element={<Home />}></Route>
+          <Route path={`${baseurl}"/contact"`} element={<Contact />}></Route>
+          <Route
+            path={`${baseurl}"/baseurl/charts-and-maps"`}
+            element={<ChartsandMaps />}
+          ></Route>
         </Routes>
       </BrowserRouter>
     </QueryClientProvider>
